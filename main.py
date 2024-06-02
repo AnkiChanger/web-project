5970451ded33410b68381ba757fa14cc7628fb7d
AnkiChanger <Androshuk.nikita7@gmail.com>
2024-06-02 19:22:43 +0300

    6

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ name = input("Напишіть ваше ім'я:\n")
 print(f'Hello {name}')
 
 while True:
-    you = input('Ти любиш сам себе? так/ні\n')
+    you = input(f'{name}, Ти любиш сам себе? так/ні\n')
     if you == 'так':
-        print('Молодець')
+        print(f'Молодець {name}')
         break
     if you == 'ні':
         print('ЩО ТІЛЬКИ Я САМ СЕБЕ ЛЮБЛЮ??')
